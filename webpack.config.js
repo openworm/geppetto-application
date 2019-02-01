bbe06f58a4c09e57ef47cbf3e2f2c29ba2f4756b
rodriguez-facundo <bruseghini.f@gmail.com>
2019-02-01 14:39:19 -0300

    update README.md

--- webpack.config.js
+++ webpack.config.js
@@ -13,29 +13,20 @@ try {
     // Failed to load config file
     console.error('\nFailed to load Geppetto Configuration')
 }
+var geppetto_client_path = 'node_modules/@geppettoengine/geppetto-client'
 
-var publicPath = ((geppettoConfig.contextPath == '/') ? geppettoConfig.contextPath : path.join("/", geppettoConfig.contextPath, "/", "geppetto/build/"));
+var publicPath = ((geppettoConfig.contextPath == '/') ? geppettoConfig.contextPath : "/" + geppettoConfig.contextPath + "/") + "geppetto/build/";
 console.log("\nThe public path (used by the main bundle when including split bundles) is: " + publicPath);
-
-// Get available theme
-// var availableTheme = "";
-// for (var theme in geppettoConfig.themes) {
-//     if (geppettoConfig.themes[theme]) {
-//         availableTheme = theme;
-//     }
-// }
-// console.log("\nEnable theme:");
-// console.log(availableTheme);
 
 var isProduction = process.argv.indexOf('-p') >= 0;
 console.log("\n Building for a " + ((isProduction) ? "production" : "development") + " environment")
 
 const availableExtensions = [
-  { from: path.resolve(__dirname, geppettoConfig.geppetto_client_path, "static/*"), to: 'static', flatten: true },
-  { from: "static/*", to: 'static', flatten: true },
+  { from: path.resolve(__dirname, geppetto_client_path, "static/*"), to: 'static', flatten: true },
 ];
 
 module.exports = function(env){
+  //geppettoConfig._webapp_folder
 	if(env!=undefined){
 		console.log(env);
 		if(env.contextPath){
@@ -60,7 +51,7 @@ module.exports = function(env){
   
 	var entries = {
         main: path.resolve(__dirname, "ComponentsInitialization.js"),
-        admin: path.resolve(__dirname, geppettoConfig.geppetto_client_path, "js/pages/admin/admin.js"),
+        admin: path.resolve(__dirname, geppetto_client_path, "js/pages/admin/admin.js"),
 	};
 
 	console.log("\nThe Webpack entries are:");
@@ -82,7 +73,7 @@ module.exports = function(env){
 	        new CopyWebpackPlugin(availableExtensions),
 	        new HtmlWebpackPlugin({
 	            filename: 'geppetto.vm',
-	            template: path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'js/pages/geppetto/geppetto.ejs'),
+	            template: path.resolve(__dirname, geppetto_client_path, 'js/pages/geppetto/geppetto.ejs'),
 	            GEPPETTO_CONFIGURATION: geppettoConfig,
 	            // chunks: ['main'] Not specifying the chunk since its not possible
 				// yet (need to go to Webpack2) to specify UTF-8 as charset without
@@ -91,7 +82,7 @@ module.exports = function(env){
 	        }),
 	        new HtmlWebpackPlugin({
 	            filename: 'admin.vm',
-	            template: path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'js/pages/admin/admin.ejs'),
+	            template: path.resolve(__dirname, geppetto_client_path, 'js/pages/admin/admin.ejs'),
               GEPPETTO_CONFIGURATION: geppettoConfig,
               // chunks: ['admin'] Not specifying the chunk since its not possible
 				// yet (need to go to Webpack2) to specify UTF-8 as charset without
@@ -100,13 +91,13 @@ module.exports = function(env){
 	        }),
 	        new HtmlWebpackPlugin({
 	            filename: 'dashboard.vm',
-	            template: path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'js/pages/dashboard/dashboard.ejs'),
+	            template: path.resolve(__dirname, geppetto_client_path, 'js/pages/dashboard/dashboard.ejs'),
 	            GEPPETTO_CONFIGURATION: geppettoConfig,
 	            chunks: []
 	        }),
 	        new HtmlWebpackPlugin({
 	            filename: '../WEB-INF/web.xml',
-	            template: path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'WEB-INF/web.ejs'),
+	            template: path.resolve(__dirname, geppetto_client_path, 'WEB-INF/web.ejs'),
 	            GEPPETTO_CONFIGURATION: geppettoConfig,
 	            chunks: []
 	        }),
@@ -120,14 +111,15 @@ module.exports = function(env){
       
 	    resolve: {
 	        alias: {
-              'geppetto-client': path.resolve(__dirname, geppettoConfig.geppetto_client_path),
-	            geppetto: path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'js/pages/geppetto/GEPPETTO.js'),
+              'Application': path.resolve(__dirname),
+              'geppetto-client': path.resolve(__dirname, geppetto_client_path),
+	            geppetto: path.resolve(__dirname, geppetto_client_path, 'js/pages/geppetto/GEPPETTO.js'),
 	            handlebars: 'handlebars/dist/handlebars.js'
 	
           },
           // symlinks: true,
           modules: [
-            path.resolve(__dirname, geppettoConfig.geppetto_client_path, 'node_modules'), 
+            path.resolve(__dirname, geppetto_client_path, 'node_modules'), 
             'node_modules'
           ],
 	        extensions: ['*', '.js', '.json'],
@@ -137,7 +129,7 @@ module.exports = function(env){
 	        rules: [
 	            {
 	                test: /\.(js|jsx)$/,
-                  exclude: [/ami.min.js/, /node_modules\/(?!(webapp00001)\/).*/], 
+                  exclude: [/ami.min.js/, /node_modules\/(?!(@geppettoengine\/geppetto-client)\/).*/], 
 	                loader: 'babel-loader',
 	                query: {
 	                    presets: [['babel-preset-env', { "modules": false }], 'stage-2', 'react']
