94d0f5436b7f1ebfc212664f851afabe4897209d
tarelli <matteo.cantarelli@gmail.com>
2019-04-30 10:51:52 +0100

    Fix linting, add geppetto-client to gitignore

--- components/Application.js
+++ components/Application.js
@@ -21,125 +21,124 @@ require('../css/Application.less');
 
 export default class Application extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor (props) {
+    super(props);
 
-        this.state = {
-        };
+    this.state = {};
 
-        this.voltage_color = this.voltage_color.bind(this);
+    this.voltage_color = this.voltage_color.bind(this);
 
-        this.passThroughDataFilter = function (entities) {
-            return entities;
-        };
-    }
-
-    voltage_color(x) {
-        x = (x + 0.07) / 0.1; // normalization
-        if (x < 0) {
-            x = 0; 
-        }
-        if (x > 1) {
-            x = 1; 
-        }
-        if (x < 0.25) {
-          return [0, x * 4, 1];
-        } else if (x < 0.5) {
-          return [0, 1, (1 - (x - 0.25) * 4)];
-        } else if (x < 0.75) {
-          return [(x - 0.5) * 4, 1, 0];
-        } else {
-          return [1, (1 - (x - 0.75) * 4), 0];
-        }
+    this.passThroughDataFilter = function (entities) {
+      return entities;
     };
+  }
 
-    /* React functions */
-
-    componentWillMount() {
-        window.voltage_color = function(x) {
-            return this.voltage_color(x);
-        }.bind(this);
+  voltage_color (x) {
+    x = (x + 0.07) / 0.1; // normalization
+    if (x < 0) {
+      x = 0; 
     }
+    if (x > 1) {
+      x = 1; 
+    }
+    if (x < 0.25) {
+      return [0, x * 4, 1];
+    } else if (x < 0.5) {
+      return [0, 1, (1 - (x - 0.25) * 4)];
+    } else if (x < 0.75) {
+      return [(x - 0.5) * 4, 1, 0];
+    } else {
+      return [1, (1 - (x - 0.75) * 4), 0];
+    }
+  }
 
-    componentDidMount() {
-        GEPPETTO.G.setIdleTimeOut(-1);
-        GEPPETTO.G.enableLocalStorage(false);
+  /* React functions */
 
-        if(this.refs.canvasRef !== undefined) {
-            this.refs.canvasRef.displayAllInstances();
-        }
+  componentWillMount () {
+    window.voltage_color = function (x) {
+      return this.voltage_color(x);
+    }.bind(this);
+  }
+
+  componentDidMount () {
+    GEPPETTO.G.setIdleTimeOut(-1);
+    GEPPETTO.G.enableLocalStorage(false);
+
+    if (this.refs.canvasRef !== undefined) {
+      this.refs.canvasRef.displayAllInstances();
+    }
         
-        if(this.refs.controlPanelRef !== undefined) {
-            this.refs.controlPanelRef.setDataFilter(this.passThroughDataFilter);
-        }
-
-        if(this.refs.spotlightRef !== undefined) {
-            this.refs.spotlightRef.addSuggestion(GEPPETTO.Spotlight.plotSample, GEPPETTO.Resources.PLAY_FLOW);
-        }
-    };
-
-    render() {
-
-        return (
-            <div style={{ height: '100%', width: '100%' }}>
-
-                <Logo
-                    logo='gpt-gpt_logo'
-                    id="geppettologo" />
-
-                <div id="github-logo">
-                    <LinkButton
-                        left={41}
-                        top={390}
-                        icon='fa fa-github' 
-                        url='https://github.com/openworm/org.geppetto' />
-                </div>
-
-                <div id="HomeButton">
-                    <Home />
-                </div>
-
-                <div id="share-button">
-                    <Share />
-                </div>
-
-                <div id="SaveButton">
-                    <SaveControl />
-                </div>
-
-                <div id="sim-toolbar">
-                    <SimulationControls />
-                </div>
-
-                <div id="foreground-toolbar">
-                    <ForegroundControls dropDown={false} />
-                </div>
-
-                <div id="sim">
-                    <Canvas
-                        id="CanvasContainer"
-                        name={"Canvas"}
-                        ref="canvasRef" />
-                </div>
-
-                <div id="spotlight" style={{ top: 0 }}>
-                    <SpotLight ref="spotlightRef" icon={"styles.Modal"} />
-                </div>
-
-                <div id="controlpanel" style={{ top: 0 }}>
-                    <ControlPanel ref="controlPanelRef" icon={"styles.Modal"} enableInfiniteScroll={true}
-                        useBuiltInFilter={true} resultsPerPage={10} enablePagination={true} />
-                </div>
-
-                <div id="footer">
-                    <div id="footerHeader">
-                        <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]} >
-                            <Console />
-                            <ExperimentsTable />
-                        </TabbedDrawer>
-                    </div>
-                </div>
-            </div>
-        );
+    if (this.refs.controlPanelRef !== undefined) {
+      this.refs.controlPanelRef.setDataFilter(this.passThroughDataFilter);
     }
+
+    if (this.refs.spotlightRef !== undefined) {
+      this.refs.spotlightRef.addSuggestion(GEPPETTO.Spotlight.plotSample, GEPPETTO.Resources.PLAY_FLOW);
+    }
+  }
+
+  render () {
+
+    return (
+      <div style={{ height: '100%', width: '100%' }}>
+
+        <Logo
+          logo='gpt-gpt_logo'
+          id="geppettologo" />
+
+        <div id="github-logo">
+          <LinkButton
+            left={41}
+            top={390}
+            icon='fa fa-github' 
+            url='https://github.com/openworm/org.geppetto' />
+        </div>
+
+        <div id="HomeButton">
+          <Home />
+        </div>
+
+        <div id="share-button">
+          <Share />
+        </div>
+
+        <div id="SaveButton">
+          <SaveControl />
+        </div>
+
+        <div id="sim-toolbar">
+          <SimulationControls />
+        </div>
+
+        <div id="foreground-toolbar">
+          <ForegroundControls dropDown={false} />
+        </div>
+
+        <div id="sim">
+          <Canvas
+            id="CanvasContainer"
+            name={"Canvas"}
+            ref="canvasRef" />
+        </div>
+
+        <div id="spotlight" style={{ top: 0 }}>
+          <SpotLight ref="spotlightRef" icon={"styles.Modal"} />
+        </div>
+
+        <div id="controlpanel" style={{ top: 0 }}>
+          <ControlPanel ref="controlPanelRef" icon={"styles.Modal"} enableInfiniteScroll={true}
+            useBuiltInFilter={true} resultsPerPage={10} enablePagination={true} />
+        </div>
+
+        <div id="footer">
+          <div id="footerHeader">
+            <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]} >
+              <Console />
+              <ExperimentsTable />
+            </TabbedDrawer>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
