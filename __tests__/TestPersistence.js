257ffb90509c6597c315375b9f6aa2409f27a7d3
walrus <walrus@compu-walrus>
2019-07-12 10:25:09 -0700

    skeleton for new tests, package.json changes because previous version didn't work in linux

---
+++ __tests__/TestPersistence.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const { TimeoutError } = require('puppeteer/Errors');
+
+import { getCommandLineArg, getUrlFromProjectId } from './cmdline.js';
+import { wait4selector, click } from './utils';
+
+import * as ST from './selectors';
+
+const COLLAPSE_WIDGET_HEIGHT = 35;
+const baseURL = getCommandLineArg('--url', 'http://live.geppetto.org');
+
+
+describe('Test Persistence Geppetto', () => {
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    
+    await page.goto(baseURL);
+  });
+
+  afterAll(async () => {
+  })
+
+  
+  describe('Test Dashboard', () => {
+    const PROJECT_IDS = [1, 3, 4, 5, 6, 8, 9, 16, 18, 58];
+    it.each(PROJECT_IDS)('Project width id %i from core bundle are present', async id => {
+      await page.waitForSelector(`div[project-id="${id}"]`);
+    })
+  })
+})
