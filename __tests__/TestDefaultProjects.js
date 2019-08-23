d3ead972707caef162cae35c9894c5d2e7881b03
jrmartin <yeshuarmartinez@gmail.com>
2019-08-23 10:41:04 -0700

    separate tests in own describe declaration, hail mary attempt

--- __tests__/TestDefaultProjects.js
+++ __tests__/TestDefaultProjects.js
@@ -21,13 +21,67 @@ describe('Test Default Projects', () => {
   });
   //describe('Test Dashboard', () => testDashboard());
   describe('Single Component HH Project', () => testSingleComponentHHProject());
-  describe('Acnet Project', () => testACNET2Project());
-  describe('C302 Network Project', () => testC302NetworkProject());
-  describe('Ca1 Project', () => testCa1Project());
-  describe('EyeWire Project', () => testEyeWireProject());
-  describe('Pharyngeal Project', () => testPharyngealProject());
-  describe('cElegansConnectome Project', () => testC302Connectome());
-  describe('cElegansMuscleModel Project', () => testPMuscleCellProject());
-  describe('cElegansPVDR Project', () => testPVDRNeuronProject());
-  // describe('Cylinders Project', () => testCylindersProject());
 });
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  //describe('Test Dashboard', () => testDashboard());
+	  describe('C302 Network Project', () => testC302NetworkProject());
+	  describe('Ca1 Project', () => testCa1Project());
+	});
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('C302 Network Project', () => testC302NetworkProject());
+	});
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('EyeWire Project', () => testEyeWireProject());
+	});
+
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('Pharyngeal Project', () => testPharyngealProject());
+	});
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('cElegansConnectome Project', () => testC302Connectome());
+
+	});
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('cElegansMuscleModel Project', () => testPMuscleCellProject());
+	  // describe('Cylinders Project', () => testCylindersProject());
+	});
+
+describe('Test Default Projects', () => {
+	  beforeAll(async () => {
+	    jest.setTimeout(200000);
+
+	  });
+	  describe('cElegansPVDR Project', () => testPVDRNeuronProject());
+	  // describe('Cylinders Project', () => testCylindersProject());
+	});
+
