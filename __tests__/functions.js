029d8b7bf902a44ba19f5405f16635e823ec1355
Afonso Pinto <afonsobspinto@gmail.com>
2019-08-26 18:32:15 +0100

    Update timeouts

--- __tests__/functions.js
+++ __tests__/functions.js
@@ -152,7 +152,7 @@ export const testSpotlight = async (page, variableName,plotName,expectButton,tes
 export const testCameraControls = async (page, expectedCameraPosition) => {
   const scheduler = [
     [zoomClicks, ST.ZOOM_BUTTON_SELECTOR, 1000],
-    [panClicks, ST.PAN_RIGHT_BUTTON_SELECTOR, 1000],
+    [panClicks, ST.PAN_RIGHT_BUTTON_SELECTOR, 1500],
     [rotateClicks, ST.ROTATE_RIGHT_BUTTON_SELECTOR, 3000]
   ];
 
@@ -175,7 +175,7 @@ export const testCameraControlsWithCanvasWidget = async (page, expectedCameraPos
 
   const scheduler = [
     [zoomClicks * 2, ST.ZOOM_BUTTON_SELECTOR, ST.ZOOM_BUTTON_CANVAS_2_SELECTOR, 1000],
-    [panClicks * 2, ST.PAN_RIGHT_BUTTON_SELECTOR, ST.PAN_RIGHT_BUTTON_CANVAS_2_SELECTOR, 1000],
+    [panClicks * 2, ST.PAN_RIGHT_BUTTON_SELECTOR, ST.PAN_RIGHT_BUTTON_CANVAS_2_SELECTOR, 1500],
     [rotateClicks * 2, ST.ROTATE_RIGHT_BUTTON_SELECTOR, ST.ROTATE_RIGHT_BUTTON_CANVAS_2_SELECTOR, 3000]
   ];
 
