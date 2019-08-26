acd179e61c4231272e6174f24e3014af1526a832
Afonso Pinto <afonsobspinto@gmail.com>
2019-08-26 14:45:31 +0100

    Test default projects

--- __tests__/TestDefaultProjects.js
+++ __tests__/TestDefaultProjects.js
@@ -20,15 +20,15 @@ describe('Test Default Projects', () => {
 
   });
 
-  // describe('Test Dashboard', () => testDashboard());
-  // describe('Single Component HH Project', () => testSingleComponentHHProject());
-  //describe('Acnet Project', () => testACNET2Project());
-   describe('C302 Network Project', () => testC302NetworkProject());
-  // describe('Ca1 Project', () => testCa1Project());
-  // describe('EyeWire Project', () => testEyeWireProject());
-  // describe('Pharyngeal Project', () => testPharyngealProject());
-  // describe('cElegansConnectome Project', () => testC302Connectome());
-  // describe('cElegansMuscleModel Project', () => testPMuscleCellProject());
-  // describe('cElegansPVDR Project', () => testPVDRNeuronProject());
+  describe('Test Dashboard', () => testDashboard());
+  describe('Single Component HH Project', () => testSingleComponentHHProject());
+  describe('Acnet Project', () => testACNET2Project());
+  describe('C302 Network Project', () => testC302NetworkProject());
+  describe('Ca1 Project', () => testCa1Project());
+  describe('EyeWire Project', () => testEyeWireProject());
+  describe('Pharyngeal Project', () => testPharyngealProject());
+  describe('cElegansConnectome Project', () => testC302Connectome());
+  describe('cElegansMuscleModel Project', () => testPMuscleCellProject());
+  describe('cElegansPVDR Project', () => testPVDRNeuronProject());
   //describe('Cylinders Project', () => testCylindersProject());
 });
