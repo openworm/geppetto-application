9be00964cdd7b642f244248501029cf72216bb02
Afonso Pinto <afonsobspinto@gmail.com>
2019-09-03 04:13:30 +0100

    Add more flags

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,1 +1,2 @@
-module.exports = { launch: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'] }, }
+module.exports = { launch: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--unlimited-storage',
+            '--full-memory-crash-report', '--force-gpu-mem-available-mb', '--disable-dev-shm-usage'] }, }
