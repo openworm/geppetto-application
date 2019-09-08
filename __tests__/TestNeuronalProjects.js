d25d1b11b18aae79271e4ac5477aa905b0ba965a
Afonso Pinto <afonsobspinto@gmail.com>
2019-09-08 02:31:19 +0100

    Add 5 test

--- __tests__/TestNeuronalProjects.js
+++ __tests__/TestNeuronalProjects.js
@@ -21,7 +21,7 @@ describe('Test Neuronal Projects', () => {
   describe('Acnet Project', () => testACNET2Project());
   describe('C302 Network Project', () => testC302NetworkProject());
   describe('Ca1 Project', () => testCa1Project());
-  // describe('cElegansPVDR Project', () => testPVDRNeuronProject());
+  describe('cElegansPVDR Project', () => testPVDRNeuronProject());
   // describe('cElegansMuscleModel Project', () => testPMuscleCellProject());
   // describe('cElegansConnectome Project', () => testC302Connectome());
 
