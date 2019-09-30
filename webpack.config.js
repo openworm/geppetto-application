4c49b08ab6c29a4d8ea4734aa00a1e1b3ec00dd5
jrmartin <yeshuarmartinez@gmail.com>
2019-09-30 11:35:46 -0700

    cleaning and comment

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,13 @@ console.log("\nThe public path (used by the main bundle when including split bun
 var isProduction = process.argv.indexOf('-p') >= 0;
 console.log("\n Building for a " + ((isProduction) ? "production" : "development") + " environment")
 
+// These lines needed to revert direction of slashes in URL/path for CSS Themes in Windows machines
 var isWin = process.platform === "win32";
-console.log("OS is windows ", isWin)
+console.log("\n Building in Windows Machine : ", isWin)
 var cssThemesPath = path.resolve(__dirname, geppettoConfig.themes)
 if ( isWin ) {
   cssThemesPath = cssThemesPath.replace(/\\/g, "/"); 
 }
-console.log('URL CSS themes path ', cssThemesPath)
 
 const availableExtensions = [
   { from: path.resolve(__dirname, geppetto_client_path, "static/*"), to: 'static', flatten: true },
