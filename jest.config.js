56212789d3de018fdb2b3c07bafca42b98c2ee30
jrmartin <yeshuarmartinez@gmail.com>
2019-10-01 16:30:02 -0700

    fix folder path

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = { 
   "preset": "jest-puppeteer", 
-  "testRegex": "(/tests/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
+  "testRegex": "(/tests/jest-puppeteer/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$"
 };
