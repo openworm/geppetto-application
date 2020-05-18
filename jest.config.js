de7a1e083a2980e9c266f079d5ef3c9cbc43c576
Dario Del Piano <dario@metacell.us>
2020-05-18 19:54:43 +0100

    #49 ignore geppetto-client tests folder, those are designed to run within geppetto-client, fixed path

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   "preset": "jest-puppeteer",
   "testRegex": "(tests/jest-puppeteer/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$",
   "testPathIgnorePatterns": [
-      "/node_modules/",
-      "<rootDir>/geppetto-client/__tests__/*"
+      "<rootDir>/node_modules/",
+      "<rootDir>/geppetto-client/",
+      "<rootDir>/geppetto-client/__tests__/"
     ]
 };
