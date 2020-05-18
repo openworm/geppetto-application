f77ae2e80c67a6da7a3a4976e5f0a9240a7aef5b
Dario Del Piano <dario@metacell.us>
2020-05-18 22:23:44 +0100

    fixing linter

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   "preset": "jest-puppeteer",
   "testRegex": "(tests/jest-puppeteer/Test.*|(\\.|/)(test|spec))\\.[jt]sx?$",
   "testPathIgnorePatterns": [
-      "<rootDir>/node_modules/",
-      "<rootDir>/geppetto-client/",
-      "<rootDir>/geppetto-client/__tests__/"
-    ]
+    "<rootDir>/node_modules/",
+    "<rootDir>/geppetto-client/",
+    "<rootDir>/geppetto-client/__tests__/"
+  ]
 };
