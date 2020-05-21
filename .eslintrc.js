5837728725fc7bcc0d5e66d0490df922beea0bd7
jrmartin <yeshuarmartinez@gmail.com>
2020-05-21 11:12:53 -0700

    attempt to fix .estlintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [
-	"./node_modules/@geppettoengine/geppetto-client/.eslintrc.js",
-    "plugin:jest/recommended"
+    "./node_modules/@geppettoengine/geppetto-client/.eslintrc.js"
   ],
   plugins: ["jest"],
   globals: {
